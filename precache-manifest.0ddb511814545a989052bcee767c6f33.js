32fc7730f99bf677aa06f3785a04c67607e14612
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-03-03 18:12:28 -0800

    Updates

---
+++ precache-manifest.0ddb511814545a989052bcee767c6f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1913b3ee277640210572f6f9b86cb5e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427defd6a941fa60122d",
+    "url": "/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "57604b455ae3077a3b6a",
+    "url": "/static/js/2.bdaee75a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.bdaee75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427defd6a941fa60122d",
+    "url": "/static/js/main.9537e4c2.chunk.js"
+  },
+  {
+    "revision": "76385abc358cc72123b6",
+    "url": "/static/js/runtime-main.ba3d8332.js"
+  }
+]);
